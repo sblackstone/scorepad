6c89120eb04ad5821825a15a3a21acece441faba
Stephen <sblackstone@gmail.com>
2018-12-24 22:12:46 -0500

    Updates

---
+++ precache-manifest.f1fc1f5116f62c266874736dead47ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49cd5ecd69fd9905f700",
+    "url": "/scorepad/static/css/main.060599db.chunk.css"
+  },
+  {
+    "revision": "49cd5ecd69fd9905f700",
+    "url": "/scorepad/static/js/main.49cd5ecd.chunk.js"
+  },
+  {
+    "revision": "7cb97ed2d58e3e59f43b",
+    "url": "/scorepad/static/css/1.ed19ce8d.chunk.css"
+  },
+  {
+    "revision": "7cb97ed2d58e3e59f43b",
+    "url": "/scorepad/static/js/1.7cb97ed2.chunk.js"
+  },
+  {
+    "revision": "fe8a71ddd0fa2f491be3",
+    "url": "/scorepad/static/js/runtime~main.fe8a71dd.js"
+  },
+  {
+    "revision": "50836b4d6c256a05304a72f2e86afbd2",
+    "url": "/scorepad/index.html"
+  }
+];
